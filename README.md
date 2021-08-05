ac828a43293d8711a7db830e4deb392aad9bd8f7
kris248 <kriskannan1712@gmail.com>
2021-08-05 22:30:56 +0530

    i guess it will work

---
+++ README.md
@@ -0,0 +1,26 @@
+# Pradhinidhi
+
+Evade internet censorship!
+
+The original Pradhinidhi is now gone, but it's now easier than ever to deploy your own copy.
+
+[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/nfriedly/Pradhinidhi)
+<!-- [![Deploy to Bluemix](https://cloud.ibm.com/devops/setup/deploy/button.png)](https://bluemix.net/deploy?repository=https://github.com/nfriedly/Pradhinidhi)
+[![Deploy to Azure](http://azuredeploy.net/deploybutton.png)](https://azuredeploy.net/)
+[![Deploy to AWS](https://oneclick.amplifyapp.com/button.svg)](https://console.aws.amazon.com/amplify/home#/deploy?repo=https://github.com/nfriedly/Pradhinidhi) -->
+
+
+## Running the website on your computer
+
+1. Install [node.js](http://nodejs.org/)
+2. [Download the code](https://github.com/nfriedly/Pradhinidhi/archive/master.zip)
+3. Unzip it
+4. Open up a terminal/command line
+5. `cd` into the directory
+6. Run `npm install` to grab the dependencies.
+7. Run `npm start` to start the server. It should spawn a new instance for each CPU core you have.
+
+(Note: running `node app.js` *will not work*. The server code is in the [Gatling](https://npmjs.org/package/gatling)
+package, which the `npm start` command calls automatically.)
+
+After that, it will be live on your computer and accessible from your computer at http://localhost:8080/ - accessing it from another computer is beyond the scope of this guide, but it is possible.
---
+++ app.js
@@ -0,0 +1,101 @@
+var http = require('http');
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
+var url = require('url');
+var querystring = require('querystring');
+var express = require('express');
+var Unblocker = require('unblocker');
+var Transform = require('stream').Transform;
+
+var app = express();
+
+var google_analytics_id = process.env.GA_ID || null;
+
+function addGa(html) {
+    if (google_analytics_id) {
+        var ga = [
+            "<script type=\"text/javascript\">",
+            "var _gaq = []; // overwrite the existing one, if any",
+            "_gaq.push(['_setAccount', '" + google_analytics_id + "']);",
+            "_gaq.push(['_trackPageview']);",
+            "(function() {",
+            "  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;",
+            "  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';",
+            "  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);",
+            "})();",
+            "</script>"
+        ].join("\n");
+        html = html.replace("</body>", ga + "\n\n</body>");
+    }
+    return html;
+}
+
+function googleAnalyticsMiddleware(data) {
+    if (data.contentType == 'text/html') {
+
+        // https://nodejs.org/api/stream.html#stream_transform
+        data.stream = data.stream.pipe(new Transform({
+            decodeStrings: false,
+            transform: function (chunk, encoding, next) {
+                this.push(addGa(chunk.toString()));
+                next();
+            }
+        }));
+    }
+}
+
+var unblockerConfig = {
+    prefix: '/proxy/',
+    responseMiddleware: [
+        googleAnalyticsMiddleware
+    ]
+};
+
+var unblocker = Unblocker(unblockerConfig)
+
+// this line must appear before any express.static calls (or anything else that sends responses)
+app.use(unblocker);
+
+// serve up static files *after* the proxy is run
+app.use('/', express.static(__dirname + '/public'));
+
+// this is for users who's form actually submitted due to JS being disabled or whatever
+app.get("/no-js", function (req, res) {
+    // grab the "url" parameter from the querystring
+    var site = querystring.parse(url.parse(req.url).query).url;
+    // and redirect the user to /proxy/url
+    res.redirect(unblockerConfig.prefix + site);
+});
+
+// start one process per cpu core + one master process
+if (cluster.isMaster) {
+    console.log('Master pradhinidhi process is running with pid', process.pid);
+
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log('worker died, pid was', worker.process.pid);
+        // start a new worker in 10 seconds, unless all workers are dead, then exit
+        setTimeout(function () {
+            if (numCPUs > 1 && Object.keys(cluster.workers).length > 0) {
+                cluster.fork();
+            } else {
+                console.log('all workers have died, exiting');
+                process.exit(code);
+            }
+        }, 10 * 1000);
+    });
+} else {
+    // Workers can share any TCP connection
+    // In this case it is an HTTP server
+    var server = http.createServer(app);
+
+    server.listen(process.env.PORT || 3000);
+
+    server.on('upgrade', unblocker.onUpgrade);
+
+    console.log('pradhinidhi worker process started with pid', process.pid);
+}
